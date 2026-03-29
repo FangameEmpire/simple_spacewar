9507cd15dc015119b4eaa3e5eb30438e183aabdf
FangameEmpire <fangameempire@gmail.ccom>
2026-03-28 17:17:11 -0700

    Can draw bounding box, but digital scope only draws points instead of lines between points

--- src/project.v
+++ src/project.v
@@ -16,12 +16,63 @@ module tt_um_simple_spacewar (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // Set inout pins to outputs
+  assign uio_oe  = 8'hFF;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+  
+  // Ship details
+  reg [7:0] ship_x [1:0];
+  reg [6:0] ship_y [1:0];
+  reg [2:0] ship_rot [1:0];
+  
+  // XY position table. Format: X, Y, Connect to next
+  reg [15:0] xy_points [15:0];
+  
+  // Assign XY positions
+  always @ (posedge clk) begin
+    // Bounding box
+    if (~rst_n) begin
+      xy_points[0] <= 16'h0;
+      xy_points[1] <= 16'h0;
+      xy_points[2] <= 16'h0;
+      xy_points[3] <= 16'h0;
+      xy_points[4] <= 16'h0;
+    end else begin
+      xy_points[0] <= {8'h00, 7'h00, 1'b1};
+      xy_points[1] <= {8'hFF, 7'h00, 1'b1};
+      xy_points[2] <= {8'hFF, 7'h3F, 1'b1};
+      xy_points[3] <= {8'h00, 7'h3F, 1'b1};
+      xy_points[4] <= {8'h00, 7'h00, 1'b0};
+    end // if
+    
+    // Ships, Lasers
+    
+    // Unused
+    xy_points[05] <= 16'h0;
+    xy_points[06] <= 16'h0;
+    xy_points[07] <= 16'h0;
+    xy_points[08] <= 16'h0;
+    xy_points[09] <= 16'h0;
+    xy_points[10] <= 16'h0;
+    xy_points[11] <= 16'h0;
+    xy_points[12] <= 16'h0;
+    xy_points[13] <= 16'h0;
+    xy_points[14] <= 16'h0;
+    xy_points[15] <= 16'h0;
+  end // always @ (posedge clk)
+  
+  // Display positions
+  reg [31:0] counter_value;
+  always @ (posedge clk) begin
+    if (~rst_n) begin
+      counter_value <= 0;
+    end else begin
+      counter_value <= counter_value + 32'b1;
+    end // if
+  end // always @ (posedge clk)
+  
+  assign {uio_out, uo_out} = xy_points[counter_value[22:19]];
 
 endmodule
